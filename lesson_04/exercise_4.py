1e1c07f5a6994a97656a3f714f3ec7b285f3c381
DubMircea <dub.mircea@gmail.com>
2021-03-21 11:38:59 +0200

    Revert "hw3"

--- lesson_04/exercise_4.py
+++
@@ -1,7 +0,0 @@
-import utils
-
-val, dt = utils.currency_rates('USD')
-print(f"курс доллара {val}, дата {dt}")
-
-val, dt = utils.currency_rates('eur')
-print(f"курс евро {val}, дата {dt}")
--- lesson_04/exercise_5.py
+++
@@ -1,9 +0,0 @@
-import utils
-import sys
-
-if len(sys.argv) > 1:
-    currency = sys.argv[1]
-    val, dt = utils.currency_rates(currency)
-    print(f'{val}, {dt}')
-else:
-    print(f'Sorry, not enough arguments')
